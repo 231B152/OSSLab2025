d060f1b08c905b0653de0266dd3fa9f99e0237cf
231B152 <231b152@juetguna.in>
2025-08-17 21:35:43 +0530

    Added solution to Lab Exercise 1

---
+++ LE1/GTN.java
@@ -0,0 +1,12 @@
+package LE1;
+
+public class GTN {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 110;
+
+        int greater = (num1 > num2) ? num1 : num2;
+
+        System.out.println("The greater number is: " + greater);
+    }
+}
